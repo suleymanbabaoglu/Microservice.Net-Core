e0fae719cca223fe870567f441be6980f996fbda
Süleyman BABAOĞLU <suleymanbabaoglu@hotmail.com>
2022-08-03 03:00:00 +0200

    Order API Part 1 Implemented

---
+++ Services/Order/Services.Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Services.Order.Application.Dtos
+{
+    public class OrderDto    {
+
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public AddressDto Address { get; set; }
+
+        public string BuyerId { get; set; }
+
+        public List<OrderItemDto> OrderItems { get; set; }
+    }
+}
---
+++ Services/Order/Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,46 @@
+﻿using Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Services.Order.Domain.OrderAggregate
+{
+    //EF Core Features
+    //--Owned Types
+    //--Shadow Property
+    //--Backing Field
+    public class Order : Entity, IAggregateRoot
+    {
+        public DateTime CreatedDate { get; private set; }
+
+        public Address Address { get; private set; }
+
+        public string BuyerId { get; private set; }
+
+
+        private readonly List<OrderItem> _orderItems;
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+
+        public Order(Address address, string buyerId)
+        {
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            BuyerId = buyerId;
+            Address = address;
+        }
+
+        public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
+        {
+            var existProduct = _orderItems.Any(p => p.ProductId == productId);
+            if (!existProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+
+        }
+
+        public decimal GetTotalPrice => _orderItems.Sum(p => p.Price);
+    }
+}
