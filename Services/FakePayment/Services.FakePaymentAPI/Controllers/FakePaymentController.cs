cf25caae3d7492f2700bf248a0a3aaccb1cc747a
Süleyman BABAOĞLU <suleymanbabaoglu@hotmail.com>
2022-08-04 03:20:32 +0200

    FakePayment API Implemented

---
+++ Services/FakePayment/Services.FakePaymentAPI/Controllers/FakePaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.ControllerBases;
+using Shared.Dtos;
+
+namespace Services.FakePaymentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment() => CreateActionResultInstance(Response<NoContent>.Success(200));
+
+    }
+}
